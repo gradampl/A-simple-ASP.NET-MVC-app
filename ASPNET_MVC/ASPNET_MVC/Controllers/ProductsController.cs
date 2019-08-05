430b60add703d5496ee164bbb9abc7f4979015d5
gradampl <gr_adam@o2.pl>
2019-08-05 23:13:52 +0200

    Added ProductsController
    and modified _Layout.cshtml, View/Products/Index.cshtml

---
+++ ASPNET_MVC/ASPNET_MVC/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNET_MVC.Models;
+
+namespace ASPNET_MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
