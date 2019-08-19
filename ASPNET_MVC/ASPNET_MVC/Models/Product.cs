bb669ea98fc8069fa36dd533f83c94cd5b2eac0e
gradampl <gr_adam@o2.pl>
2019-08-19 11:48:07 +0200

    JS validation messages disabled

--- ASPNET_MVC/ASPNET_MVC/Models/Product.cs
+++ ASPNET_MVC/ASPNET_MVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace ASPNET_MVC.Models
 {
@@ -9,7 +10,7 @@ namespace ASPNET_MVC.Models
 
         [Required(ErrorMessage = "To pole jest wymagane.")]
         public string Description { get; set; }
-
+        
         public decimal? Price { get; set; }
 
         public virtual Category Category { get; set; }
